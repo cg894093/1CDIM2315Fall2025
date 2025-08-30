3ed063ef4192de259e4820b0e082bf9cbd541e2e
cg894093 <cggarcia4@buffs.wtamu.edu>
2025-08-29 20:32:19 -0500

    Submit new Homework1

---
+++ Homework1.cs.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define values
+        double X=2.5;
+        double Y=3.3;
+
+        // Calculate Z
+        double Z=4*X*X+3*Y;
+
+        // Print result with formatted string
+        Console.WriteLine($"When X={X} and Y={Y}, Z={Z:F2}");
+
+    }
+}
